aab1d5803352a46f105e7cb81a451a8a7b0b5057
nrsat <31095487+nrsat@users.noreply.github.com>
2021-05-12 12:12:02 +0900

    1.0.2

--- utils.py
+++ utils.py
@@ -1,20 +1,20 @@
-def returnPos(bicam_ind):
+def returnPos(cam_ind):
     pos1 = []
     tmppos = []
     inRange=False
-    for a in range(0, len(bicam_ind)):
-        if a+1<len(bicam_ind):
-            if (bicam_ind[a+1]-bicam_ind[a]==1) and inRange==False:
+    for a in range(0, len(cam_ind)):
+        if a+1<len(cam_ind):
+            if (cam_ind[a+1]-cam_ind[a]==1) and inRange==False:
                 inRange=True
-                tmppos.append(bicam_ind[a])
-            elif inRange==True and (bicam_ind[a+1]-bicam_ind[a]==1):
+                tmppos.append(cam_ind[a])
+            elif inRange==True and (cam_ind[a+1]-cam_ind[a]==1):
                 pass
-            elif inRange==True and (bicam_ind[a+1]-bicam_ind[a]!=1):
-                tmppos.append(bicam_ind[a])
+            elif inRange==True and (cam_ind[a+1]-cam_ind[a]!=1):
+                tmppos.append(cam_ind[a])
                 pos1.append(tmppos)
                 tmppos = []
                 inRange=False
         elif inRange==True:
-                tmppos.append(bicam_ind[a])
+                tmppos.append(cam_ind[a])
                 pos1.append(tmppos)
     return pos1
